bfc2d5543dee4c069faaf2cda884182064225e01
maddstyle <michaela.decamara@gmail.com>
2021-11-29 00:37:02 -0500

    Add Task Functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,27 +1,30 @@
-import PropTypes from 'prop-types'
+import * as React from "react";
+import PropTypes from "prop-types";
 
 const Button = ({ color, text }) => {
   const onClick = () => {
-    console.log('click')
-  }
+    console.log("click");
+  };
 
-  return  ( 
-  <button 
-  onClick={onClick}
-  style={{backgroundColor: color}} className='btn'>
-  {text}
-  </button>
-  )
-}
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className="btn"
+    >
+      {text}
+    </button>
+  );
+};
 
 Button.defaultProps = {
-  color: 'steelblue'
-}
+  color: "steelblue"
+};
 
 Button.propTypes = {
   text: PropTypes.string,
   color: PropTypes.string,
   onClick: PropTypes.func
-}
+};
 
-export default Button
+export default Button;
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,19 @@
-import {faTimes} from 'react-icons/fa'
+import * as React from 'react';
+import { FaTimes } from 'react-icons/fa';
 
-const Task = ({task}) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className='task' >
-      <h3>{task.text} <FaTimes />
+    <div 
+      className={`task ${task.reminder ? 'reminder' : ''}`} 
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text} <FaTimes style={{ color:'red', cursor: 'pointer' }} 
+        onClick={() => onDelete(task.id)} />
       </h3>
       <p>{task.day}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
